4f7559db40bd1adf259b49fcd1af4608f1bb07c1
alanamarzullo <140118130+alanamarzullo@users.noreply.github.com>
2024-03-20 22:37:03 -0300

    Add files via upload

---
+++ Caso_1_Rev_B.py
@@ -0,0 +1,103 @@
+import streamlit as st
+from scipy.integrate import odeint
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.animation import FuncAnimation
+from seaborn.palettes import blend_palette
+
+
+def run_simulation(L, r, n, m, Cp, rho, Ti, T0, q_fluxo, t_final, dt):
+    dx = L / n
+    x = np.linspace(dx/2, L-dx/2, n)
+    T = np.ones(n) * T0
+    t = np.arange(0, t_final, dt)
+    
+    # Criando a figura para o gráfico em regime permanente
+    fig_permanente = plt.figure(figsize=(5, 4))
+
+    #Criando figura para a animação
+    fig_animacao = plt.figure(figsize=(5, 4))
+
+    # Função que define a EDO para a variação da temperatura
+    def dTdt_function(T, t):
+        dTdt = np.zeros(n)
+        dTdt[1:n] = (m*Cp*(T[0:n-1]-T[1:n])+q_fluxo*2*np.pi*r*dx)/(rho*Cp*dx*np.pi*r**2)
+        dTdt[0] = (m*Cp*(Ti-T[0])+q_fluxo*2*np.pi*r*dx)/(rho*Cp*dx*np.pi*r**2)
+        return dTdt
+    
+    # Resolvendo a EDO usando o odeint
+    T_out = odeint(dTdt_function, T, t)
+    T_out = T_out - 273.15
+    
+    # Criação do DataFrame
+    df_Temp = pd.DataFrame(np.array(T_out), columns=x)
+    
+    # Criando uma paleta de cores
+    paleta_calor = blend_palette(['yellow', 'orange','red'], as_cmap=True, n_colors=100)
+    
+    # Função que atualiza o plot
+    def update_plot(t):
+        plt.clf()
+        line = pd.DataFrame(df_Temp.iloc[t, :]).T
+        sns.heatmap(line, cmap=paleta_calor)
+        plt.title(f'Tempo: {t} (s)')
+        
+    # Criando a animação
+    ani = FuncAnimation(fig_animacao, update_plot, frames=df_Temp.shape[0], repeat=False)
+
+    # Salvar a animação como gif
+    ani.save('Variação da Temperatura - Caso I.gif', writer='pillow', fps=10)
+    
+    # Exibindo a simulação
+    with st.expander("Visualização da Simulação em tempo real (Clique aqui para ver)"):
+        st.image('Variação da Temperatura - Caso I.gif')
+
+    # Inicialização de variáveis
+    t_teste = dt
+    T_permanente_anterior = np.inf
+    T_permanente_atual = np.zeros(n)
+
+    # Loop para testar diferentes tempos até que o regime permanente seja alcançado
+    while np.max(np.abs(T_permanente_atual - T_permanente_anterior)) > 0.01:  # Critério de convergência
+        T_permanente_anterior = T_permanente_atual.copy()
+        
+        # Resolvendo a EDO usando o odeint para o regime permanente
+        T_permanente = odeint(dTdt_function, T, [0, t_teste])[-1]
+        T_permanente_atual = T_permanente
+
+        # Incrementando o tempo de teste
+        t_teste += dt
+    
+    #Exibindo o gráfico de variação da temperatura ao longo do comprimento em regime permanente
+    plt.figure(fig_permanente)
+    plt.plot(x, T_permanente_atual, color='blue')  
+    plt.xlabel('Comprimento (x)')
+    plt.ylabel('Temperatura (°C)')
+    st.pyplot(plt)
+
+st.title('Simulação de um trocador de calor tubular simples')
+st.write('Este site simula um trocador de calor tubular simples que aquece um fluido conforme o mesmo passa por ele. Ao rodar a simulação, você poderá visualizar o perfil de temperatura no trocador de calor conforme o tempo passa. Você também poderá visualizar as temperaturas em regime permanente ao longo do comprimento do trocador.')
+
+# Carregar a imagem
+st.image('Caso 1.png', use_column_width=True)
+st.write('Figura exemplificando o trocador. Autoria própria.')
+
+# Valores input
+L = st.number_input('Comprimento do tubo (m)', min_value=0)
+r = st.number_input('Raio do tubo (m)', min_value=0.0)
+n = st.number_input('Número de nós para a discritização', min_value=1)
+m = st.number_input('Vazão Mássica (kg/s)', min_value=0.0)
+Cp = st.number_input('Capacidade de calor específico do fluido (J/kg.K)', min_value=0.0)
+rho = st.number_input('Massa específica do fluido (kg/m³)', min_value=0.0)
+Ti = st.number_input('Temperatura de entrada do fluido (K)')
+T0 = st.number_input('Temperatura inicial do trocador (K)')
+q_fluxo = st.number_input('Fluxo de calor (W/m²)', min_value=0.0)
+t_final = st.number_input('Tempo de simulação (s)', min_value=0.0)
+dt = st.number_input('Passo de tempo (s)', min_value=0.0)
+
+if st.button('Rodar Simulação'):
+    run_simulation(L, r, n, m, Cp, rho, Ti, T0, q_fluxo, t_final, dt)
+elif st.button('Rodar exemplo padrão'):
+    run_simulation(10, 0.1, 100, 0.1, 4180, 1000, 200, 500, 5000, 1000, 1)
