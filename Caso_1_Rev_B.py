a1a0842c252049e1e389c5c376c317ddd8cfd4f9
alanamarzullo <140118130+alanamarzullo@users.noreply.github.com>
2024-06-14 12:10:00 -0300

    Update Caso_1_Rev_B.py

--- Caso_1_Rev_B.py
+++ Caso_1_Rev_B.py
@@ -73,7 +73,7 @@ st.write('Uma aplicação para este caso são as serpentinas utilizadas em siste
 st.write('Este caso também pode representar qualquer tubulação industrial utilizada para aquecer fluidos através de uma fonte externa de calor.')
 st.write('Este simulador utiliza a seguinte equação de balanço de energia para o fluido que passa pelo trocador, considerando o princípio de conservação de energia:')
 st.image('Equacao Caso 1.jpg', use_column_width=True)
-st.write('ATENÇÃO: Ao final desta página, você também encontrará um botão que roda a simulação com um exemplo pré-definido ("Rodar exemplo padrão"). Este exemplo leva em torno de 30 segundos para rodar, a depender da sua velocidade de conexão. Caso queira usar seus próprios valores, use o botão "Rodar simulação" e recomenda-se utilizar um número de nós pelo menos igual ou maior que 10, para melhor visualização dos resultados.')
+st.write('ATENÇÃO: Ao final desta página, você também encontrará um botão que roda a simulação com um exemplo pré-definido ("Rodar exemplo padrão"). Este exemplo leva em torno de 30 segundos para rodar, a depender da sua velocidade de conexão. Caso queira usar seus próprios valores, use o botão "Rodar simulação" e recomenda-se utilizar um número de nós entre 10 e 30, a depender do exemplo específico utilizado.')
 
 # Valores input
 L = st.number_input('Comprimento do tubo (m)', min_value=0.0)
